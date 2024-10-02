f0e9f1237db48a831ed555911626daed1bee9d30
sujin <soojin9877@naver.com>
2024-10-02 13:32:01 +0900

    chore: 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,23 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const notoSansKR = Noto_Sans_KR({
+    weight: ["400", "700"],
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "임수진 | 프론트엔드 개발자",
-  description: "프론트엔드 개발자 임수진 포트폴리오",
-  robots: "noindex, nofollow",
+    title: "임수진 | 프론트엔드 개발자",
+    description: "프론트엔드 개발자 임수진 포트폴리오",
+    robots: "noindex, nofollow",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    return (
+        <html lang="ko">
+            <body className={notoSansKR.className}>{children}</body>
+        </html>
+    );
+};
+export default RootLayout;
