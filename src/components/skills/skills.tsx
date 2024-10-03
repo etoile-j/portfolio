7b7da3fad9cda54595d2d65e867440da6917fe3c
sujin <soojin9877@naver.com>
2024-10-03 15:58:26 +0900

    feat: skills 부분 구현

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+const feIcons = [
+    "HTML",
+    "CSS",
+    "Sass",
+    "StyledComponents",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Jotai",
+    "Jest",
+];
+
+const etcIcons = ["Git", "Github", "GithubActions", "Notion", "Figma", "Jira"];
+
+const Skills = () => {
+    return (
+        <section>
+            <h2>Skills</h2>
+            <div className="drop-shadow">
+                <ul className="flex justify-between">
+                    {feIcons.map((feIcon) => {
+                        return (
+                            <li key={feIcon}>
+                                <Image
+                                    src={`/img/icon/${feIcon}.svg`}
+                                    alt={feIcon}
+                                    title={feIcon}
+                                    width={64}
+                                    height={64}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className="pt-8">
+                    <ul className="flex justify-between">
+                        {etcIcons.map((etcIcon) => {
+                            return (
+                                <li key={etcIcon}>
+                                    <Image
+                                        src={`/img/icon/${etcIcon}.svg`}
+                                        alt={etcIcon}
+                                        title={etcIcon}
+                                        width={64}
+                                        height={64}
+                                    />
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+};
+export default Skills;
