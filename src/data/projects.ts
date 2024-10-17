722ef3059668b09074b1d0b38d1282f11b76b172
sujin <soojin9877@naver.com>
2024-10-17 20:33:53 +0900

    fix: 분리된 타입에 맞게 데이터 구조 변경

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,21 +1,22 @@
 export const meetsin = {
     title: "meetsin",
     titleDescription: "실시간 비대면 스터디룸 서비스",
-    period: "2023.09 - 진행 중",
-    teamInfo: "팀 프로젝트 (3인) | 프론트엔드, 백엔드, 디자인 담당",
-    skills: {
-        common: ["TypeScript", "Socket.IO"],
-        frontend: ["Next.js", "TanStack Query", "Jotai", "SCSS"],
-        backend: ["NestJS", "MongoDB", "Mongoose", "AWS EC2", "Nginx", "Github Actions"],
+    specs: {
+        period: "2023.09 - 진행 중",
+        teamInfo: "팀 프로젝트 (3인) | 프론트엔드, 백엔드, 디자인 담당",
+        skills: {
+            common: ["TypeScript", "Socket.IO"],
+            frontend: ["Next.js", "TanStack Query", "Jotai", "SCSS"],
+            backend: ["NestJS", "MongoDB", "Mongoose", "AWS EC2", "Nginx", "Github Actions"],
+        },
     },
-
     highlights: [
         {
             title: "제목",
             description: "내용",
         },
     ],
-    projectLinks: {
+    links: {
         github: "https://github.com/FE-Ocean/meetsin",
         figma: "",
     },
@@ -24,19 +25,21 @@ export const meetsin = {
 export const ourshop = {
     title: "OUR-SHOP",
     titleDescription: "오픈마켓 형태의 온라인 쇼핑몰",
-    period: "2022.09-11 (2개월)",
-    teamInfo: "개인 프로젝트",
-    skills: {
-        all: [
-            "React",
-            "TypeScript",
-            "Styled-Components",
-            "React-Query",
-            "React-Testing-Library",
-            "Jest",
-            "MSW",
-            "Github Actions",
-        ],
+    specs: {
+        period: "2022.09-11 (2개월)",
+        teamInfo: "개인 프로젝트",
+        skills: {
+            all: [
+                "React",
+                "TypeScript",
+                "Styled-Components",
+                "React-Query",
+                "React-Testing-Library",
+                "Jest",
+                "MSW",
+                "Github Actions",
+            ],
+        },
     },
     highlights: [
         {
@@ -44,7 +47,7 @@ export const ourshop = {
             description: "내용",
         },
     ],
-    projectLinks: {
+    links: {
         github: "https://github.com/etoile-j/OUR-SHOP",
     },
 };
@@ -52,16 +55,18 @@ export const ourshop = {
 export const whobego = {
     title: "who-be-go",
     titleDescription: "후기 무비 고 | 나만의 영화 감상 기록장",
-    period: "2022.08 (2주)",
-    teamInfo: "팀 프로젝트 (6인) | 프론트엔드 담당",
-    skills: { all: ["TypeScript", "HTML", "SCSS"] },
+    specs: {
+        period: "2022.08 (2주)",
+        teamInfo: "팀 프로젝트 (6인) | 프론트엔드 담당",
+        skills: { all: ["TypeScript", "HTML", "SCSS"] },
+    },
     highlights: [
         {
             title: "제목",
             description: "내용",
         },
     ],
-    projectLinks: {
+    links: {
         github: "https://github.com/FE-Ocean/who-be-go",
         liveService: "https://who-be-go.netlify.app",
     },
