73dedc0a790997da16eb61578d2197a8bed9966e
sujin <soojin9877@naver.com>
2024-10-17 19:02:10 +0900

    feat: footer 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import Intro from "@/components/intro/intro";
 import Skills from "@/components/skills/skills";
 import Project from "@/components/project/project";
+import Footer from "@/components/footer/footer";
 
 const Home = () => {
     return (
-        <main>
-            <Intro />
-            <div className="md:mx-24 xl:mx-32 text-center">
-                <Skills />
-                <Project />
-            </div>
-        </main>
+        <>
+            <main>
+                <Intro />
+                <div className="md:mx-24 xl:mx-32 text-center">
+                    <Skills />
+                    <Project />
+                </div>
+            </main>
+            <Footer />
+        </>
     );
 };
 export default Home;
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="text-center bg-gradient-to-r from-violet-400 via-blue-200 to-fuchsia-300">
+            <p className="py-5">ⓒ 2014. Sujin Lim all rights reserved.</p>
+        </footer>
+    );
+};
+export default Footer;
