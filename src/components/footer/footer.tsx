6ecdd7b772be9b39cfba1e7a83d69c9ecb04bafd
sujin <soojin9877@naver.com>
2024-10-21 19:10:05 +0900

    fix: footer 오타 수정

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="text-center bg-gradient-to-r from-violet-400 via-blue-200 to-fuchsia-300">
-            <p className="py-5">ⓒ 2014. Sujin Lim all rights reserved.</p>
+            <p className="py-5">ⓒ 2024. Sujin Lim all rights reserved.</p>
         </footer>
     );
 };
