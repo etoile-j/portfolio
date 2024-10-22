180eb56a6cc1d74dbe68d0db89fd24498b271d52
sujin <soojin9877@naver.com>
2024-10-22 19:14:22 +0900

    fix: 프로젝트 태그 변경

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -2,6 +2,7 @@
 import { useRef } from "react";
 import ProjectCard from "./projectCard";
 import useSlideUp from "@/hooks/useSlideUp";
+// import { projects } from "@/data";
 import { meetsin, ourshop, whobego } from "@/data";
 
 const Project = () => {
@@ -21,24 +22,24 @@ const Project = () => {
                 Project
             </h2>
             <div className="text-left lg:overflow-hidden">
-                <div
+                <article
                     className={`opacity-0 ${isVisibleMeetsin ? "animate-slideUp" : ""}`}
                     ref={meetsinRef}
                 >
                     <ProjectCard projectData={meetsin} />
-                </div>
-                <div
+                </article>
+                <article
                     className={`opacity-0 ${isVisibleOurshop ? "animate-slideUp" : ""}`}
                     ref={ourshopRef}
                 >
                     <ProjectCard projectData={ourshop} />
-                </div>
-                <div
+                </article>
+                <article
                     className={`opacity-0 ${isVisibleWhobego ? "animate-slideUp" : ""}`}
                     ref={whobegoRef}
                 >
                     <ProjectCard projectData={whobego} />
-                </div>
+                </article>
             </div>
         </section>
     );
