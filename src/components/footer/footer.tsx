ed925d29046ac3eb57496c9d1b580536e7fc9906
sujin <soojin9877@naver.com>
2024-10-22 18:58:09 +0900

    refactor: gradient 글로벌 설정 및 적용

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="text-center bg-gradient-to-r from-violet-400 via-blue-200 to-fuchsia-300">
+        <footer className="text-center key-gradient">
             <p className="py-5">ⓒ 2024. Sujin Lim all rights reserved.</p>
         </footer>
     );
