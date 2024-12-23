c4267a6c51d889e090f31bb8f62f0dc55ba52619
sujin <soojin9877@naver.com>
2024-12-23 18:11:48 +0900

    chore: 업데이트 월 변경

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -37,7 +37,7 @@ const Intro = () => {
                             <br /> 개발자 임수진입니다.
                         </p>
                     </div>
-                    <p className="pl-2 text-gray-200">2024.11.</p>
+                    <p className="pl-2 text-gray-200">2024.12.</p>
                 </div>
             </div>
             <Image
